8f098be6f9791b4cfd4f7d099475f60231680451
LOGANM16 <116862189+LOGANM16@users.noreply.github.com>
2024-09-02 13:37:02 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+// import { useState } from 'react'
+// import './App.css'
+// import {Routes, Route, Navigate} from "react-router-dom"
+// import SearchResults from './SearchResults'
+// import Transaction from "./Transaction"
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <Routes>
+//       <Route path="/" element={<SearchResults/>} />
+//       <Route path="/transaction" element={<Transaction />} />
+//       {/* <Route path="/m" element={< />} /> */}
+//     </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SearchResults from './SearchResults';
+import Transaction from './Transaction';
+import SlideUpPage from './SlideUpPage'; // Import the SlideUpPage component
+import EligibilitySteps from './EligibilitySteps';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="relative">
+      <Routes>
+        <Route path="/" element={<SearchResults />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/paywithcircle" element={<EligibilitySteps />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
